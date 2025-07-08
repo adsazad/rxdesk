efe00350913dfb73946c8f7b2666498088610d15
Arashdeep Singh <arashdeeps2004@gmail.com>
2025-07-08 17:36:09 +0530

    .

--- lib/home.dart
+++ lib/home.dart
@@ -192,7 +192,7 @@ class _HomeState extends State<Home> {
       },
     ];
 
-    startTestLoop(); // Start the test loop
+    // startTestLoop(); // Start the test loop
   }
 
   initFunc() async {
